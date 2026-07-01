9f66f15fb80ea68b68fae03630abe245bc92398c
MX.55 <83025703+AAAA0001@users.noreply.github.com>
2026-07-01 11:14:34 +0200

    Add files via upload

---
+++ warframe_damage_calculator/__init__.py
@@ -0,0 +1,12 @@
+from .mechanics.dist import dist
+from .models import Upgrade, Melee, Primary, Secondary
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "dist",
+    "Upgrade",
+    "Melee",
+    "Primary",
+    "Secondary"
+]
---
+++ warframe_damage_calculator/models/__init__.py
@@ -0,0 +1,15 @@
+from .weapon import Weapon
+from .ranged import Ranged
+from .melee import Melee
+from .primary import Primary
+from .secondary import Secondary
+from .upgrade import Upgrade
+
+__all__ = [
+    "Weapon",
+    "Ranged",
+    "Melee",
+    "Primary",
+    "Secondary",
+    "Upgrade"
+]
