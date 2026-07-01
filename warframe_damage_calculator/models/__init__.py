90f7c3cf7eb9b16275e0a7aed022c9d2746e3ad3
MX.55 <83025703+AAAA0001@users.noreply.github.com>
2026-07-02 00:24:54 +0200

    Add files via upload

---
+++ warframe_damage_calculator/models/__init__.py
@@ -0,0 +1,12 @@
+from .states import WeaponState, MeleeState, RangedState, PrimaryState, SecondaryState
+from .dist import dist
+
+
+__all__ = [
+    "WeaponState",
+    "MeleeState",
+    "RangedState",
+    "PrimaryState",
+    "SecondaryState",
+    "dist",
+]
---
+++ warframe_damage_calculator/utils/__init__.py
@@ -0,0 +1,14 @@
+from ..utils.constants import DOT_MULTIPLIERS, PHYSICAL_TYPES, ELEMENTAL_TYPES, ELEMENTAL_COMBINATIONS, DAMAGE_TYPES, DAMAGE_TYPE_ORDER
+from ..utils.functions import true_round, clamp
+
+
+__all__ = [
+    "DOT_MULTIPLIERS",
+    "PHYSICAL_TYPES",
+    "ELEMENTAL_TYPES",
+    "ELEMENTAL_COMBINATIONS",
+    "DAMAGE_TYPES",
+    "DAMAGE_TYPE_ORDER",
+    "true_round",
+    "clamp",
+]
