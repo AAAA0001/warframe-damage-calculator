5b38742094bcdc81b81626dd67914f872246815a
MX.55 <83025703+AAAA0001@users.noreply.github.com>
2026-07-02 16:40:14 +0200

    Delete warframe_damage_calculator directory

--- warframe_damage_calculator/utils/constants.py
+++
@@ -1,9 +0,0 @@
-from .distargs import DistArgs
-
-
-DOT_MULTIPLIERS = (("slash", 2.1), ("heat", 3.0), ("toxin", 3.0), ("electricity", 3.0), ("gas", 3.0))
-PHYSICAL_TYPES = ("impact", "puncture", "slash")
-ELEMENTAL_TYPES = ("cold", "electricity", "heat", "toxin")
-DAMAGE_TYPES = tuple(DistArgs.__annotations__)
-ELEMENTAL_COMBINATIONS = {frozenset(("cold", "heat")): "blast", frozenset(("electricity", "toxin")): "corrosive", frozenset(("heat", "toxin")): "gas", frozenset(("cold", "electricity")): "magnetic", frozenset(("electricity", "heat")): "radiation", frozenset(("cold", "toxin")): "viral"}
-DAMAGE_TYPE_ORDER = dict(zip(DAMAGE_TYPES, range(len(DAMAGE_TYPES))))
